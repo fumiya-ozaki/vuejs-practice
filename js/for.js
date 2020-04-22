aa3597682acded26618d65f074ade5c35c1694cc
fumiya-ozaki <fumiya0414@gmail.com>
2020-04-22 12:09:02 +0900

    4_21

---
+++ js/for.js
@@ -0,0 +1,17 @@
+new Vue({
+  el:"#app",
+  data:{
+    books:[
+      {
+        isbn: '978-4-7981-5757-3',
+        title: 'JavaScript逆引きレシピ',
+        price: 2800
+      },
+      {
+        isbn: '978-4-7981-5757-4',
+        title: 'ruby逆引きレシピ',
+        price: 2000
+      }
+    ]
+  }
+});
---
+++ js/for_index.js
@@ -0,0 +1,22 @@
+new Vue({
+  el:"#app",
+  data:{
+    books:[
+      {
+        isbn: '978-4-7981-5757-3',
+        title: 'JavaScript逆引きレシピ',
+        price: 2800
+      },
+      {
+        isbn: '978-4-7981-5757-4',
+        title: 'html逆引きレシピ',
+        price: 2000
+      },
+      {
+        isbn: '978-4-7981-5757-4',
+        title: 'ruby逆引きレシピ',
+        price: 2000
+      }
+    ]
+  }
+});
